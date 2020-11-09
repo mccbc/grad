87557d44b9413ffd5aa87d1238defd82f60fda3e
mccbc <cmcclellan1010@gmail.com>
2020-11-08 23:07:52 -0500

    edited commit count to include special message for end of month, optional email parameter for double checking daily

--- commit_count.py
+++ commit_count.py
@@ -4,46 +4,25 @@ from email.message import EmailMessage
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-p', '--password', type=str, help='Gmail password for "From" address')
+parser.add_argument(
+    '-p',
+    '--password',
+    type=str,
+    help='Gmail password for "From" address')
+parser.add_argument(
+    '-e',
+    '--email',
+    type=bool,
+    help='Send an email if there are no new commits')
 args = parser.parse_args()
 
 ###
 your_username = 'mccbc'
 ###
 
-# Scrape HTML from github
-with urllib.request.urlopen('https://github.com/'+your_username) as f:
-    lines = [line.decode('utf-8') for line in f.readlines()]
-    f.close()
 
-# Check the number of commits this month from the scraped HTML
-commits = 0 
-for i, line in enumerate(lines): 
-    if 'commit' in line and '>' not in line: 
-        try: 
-            print('Repository: ', lines[i-1].split('href="/'+your_username+'/')[1].split('/commits?')[0]) 
-            print(lines[i]) 
-            commits += int(lines[i].lstrip()[0])
-        except: 
-            pass 
-
-# Load running count from last time this script was run
-with open('count.txt', 'r') as f:
-    previous_commits = int(f.read()) # Will need to reset this every month...
-    new_commits = commits - previous_commits
-
-with open('count.txt', 'w') as f:
-    f.write(str(commits))
-
-# Print some statistics
-print('Total commits this month: {}'.format(commits))
-print('Previous commits: {}'.format(previous_commits))
-print('New commits: {}'.format(new_commits))
-
-if new_commits == 0:
-    msg = EmailMessage()
-    msg.set_content('You have not committed to any repositories today!')
-    msg['Subject'] = '['+your_username+'] Git Commit Reminder'
+def send_message(msg):
+    msg['Subject'] = '[' + your_username + '] Git Commit Reminder'
     msg['From'] = 'cmcclellan1010@gmail.com'
     msg['To'] = ['cmcclellan1010@gmail.com']
 
@@ -51,3 +30,45 @@ if new_commits == 0:
     s.login(msg['From'], args.password)
     s.sendmail(msg['From'], [msg['To']], msg.as_string())
     s.quit()
+
+
+# Scrape HTML from github
+with urllib.request.urlopen('https://github.com/' + your_username) as f:
+    lines = [line.decode('utf-8') for line in f.readlines()]
+    f.close()
+
+# Check the number of commits this month from the scraped HTML
+commits = 0
+for i, line in enumerate(lines):
+    if 'commit' in line and '>' not in line:
+        try:
+            print('Repository: ', lines[i - 1].split('href="/' + your_username
+                   + '/')[1].split('/commits?')[0])
+            print(lines[i])
+            commits += int(lines[i].lstrip()[0])
+        except BaseException:
+            pass
+
+# Load running count from last time this script was run
+with open('count.txt', 'r') as f:
+    previous_commits = int(f.read())  # Will need to reset this every month...
+    new_commits = commits - previous_commits
+
+with open('count.txt', 'w') as f:
+    f.write(str(commits))
+
+if args.email:
+    # Print some statistics
+    print('Total commits this month: {}'.format(commits))
+    print('Previous commits: {}'.format(previous_commits))
+    print('New commits: {}'.format(new_commits))
+
+    msg = EmailMessage()
+    if new_commits == 0:
+        msg.set_content('You have not committed to any repositories today!')
+        send_message(msg)
+    elif new_commis < 0:
+        msg.set_content('The running total needs to be reset for this month.')
+        send_message(msg)
+    else:
+        pass
