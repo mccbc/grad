015e6966a003c61256087c673c14893cde947fe7
mccbc <cmcclellan1010@gmail.com>
2020-11-19 21:40:24 -0500

    adding proposals subdirectory

--- 1290/crosscompare.py
+++ 1290/crosscompare.py
@@ -9,17 +9,19 @@ args = parser.parse_args()
 df = pd.read_excel('roster.xlsx', index_col=0, header=None)
 names = list(df.index[4:])
 
+ok_list = ['wang, emma', 'smith, catie claire', 'rodriguez, angie', 'nigam, krutik']
 incomplete = []
 
 with open(args.inputfile) as f:
     lines = f.readlines()
     for name in names[2:]:
-
         # BEGIN SEARCH
         passed = False
 
         # Last name match
         if any(name.split(', ')[0].lower() in line.lower() and name.split(', ')[1].split(' ')[0].lower() in line.lower() for line in lines):
+            passed = True
+        elif name.lower() in ok_list:
             passed = True
 
         # Print hits
